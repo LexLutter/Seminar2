99d92a08b5b17d369978f3d85e43bdab3d50668c
LexLuter <gae_cbr@mail.ru>
2023-02-20 21:41:14 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int i = 0;
+int result = 0;
+while (i == 0){
+    Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 0) number = number * (-1);
+    if (number > 99 & number < 1000){
+        result = (number % 100) / 10;
+        Console.WriteLine($"Вторая цифра заданного числа: {result}");
+        i++;
+    }
+    else Console.WriteLine($"Вы ввели некорректное число.");
+}
